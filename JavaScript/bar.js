083cea9432a209d24b0907b5bd9e884d10eaacf2
OttoOikarinen <oikarinenotto@gmail.com>
2023-09-05 11:27:26 +0300

    Week 1 exercises done.

--- JavaScript/bar.js
+++ JavaScript/bar.js
@@ -1,9 +1,24 @@
-function addHelloWorld() {
+function workingJS() {
     const addHelloWorldButton = document.getElementById("my-button");
+    const addTextButton = document.getElementById("add-element");
 
     addHelloWorldButton.addEventListener("click", function() {
         console.log("Hello world");
+
+        let title = document.getElementById("hello-world");
+        title.innerText = "Moi maailma";
     })
+
+    addTextButton.addEventListener("click", function() {
+        console.log("Works!");
+        
+        const list = document.getElementById("LOTR-list");
+
+        let newText = document.createElement("li");
+        newText.innerText = document.getElementById("text-area").value;
+        list.appendChild(newText);
+    })
+    
 }
 
-addHelloWorld();
+workingJS();
